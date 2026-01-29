8e099de21ccf92ef816e9a1792ea23fd4bfcd76e
shashwat12jha <shashwat1227@gmail.com>
2026-01-29 10:51:35 +0000

    Strings

--- .devcontainer/01_Basica/test.js
+++ .devcontainer/01_Basica/test.js
@@ -1,1 +1,11 @@
 console.log("Shashwat")
+const name = "Shashwat"
+let repoCount = 3;
+console.log(name+repoCount);
+console.log(`My name is ${name} and my repoCount is ${repoCount}`);
+const gameName = new String("Temple Run");
+const newgameName = new String(gameName.substring(2,8))
+console.log(newgameName);
+console.log(gameName);
+
+
