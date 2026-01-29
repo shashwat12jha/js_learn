9f9619984b875ac96679f0ec15402c4554d58190
shashwat12jha <shashwat1227@gmail.com>
2026-01-29 12:45:22 +0000

    Arrays seekho

---
+++ .devcontainer/01_Basica/array/basics.js
@@ -0,0 +1,26 @@
+const arr = [0,1,2,3,4,5,6];
+const brr = [0,6];
+arr.concat(brr);
+arr.push(brr);
+console.log(arr.flat(Infinity))
+// console.log(arr);
+// const crr =[... arr ,... brr]
+// console.log(crr);
+
+
+// // console.log(arr);
+// // arr.unshift(9)
+// // arr.shift();
+// // console.log(arr);
+// const newArr= arr.join();
+// // console.log(arr);
+// // console.log(newArr);
+// // console.log("A", newArr);
+// const myn=arr.slice(1,3);
+// console.log(myn);
+// const myn1=arr.splice(1,3);
+// console.log(arr);
+// console.log(myn1);
+
+
+
--- .devcontainer/01_Basica/test.js
+++ .devcontainer/01_Basica/test.js
@@ -7,5 +7,12 @@ const gameName = new String("Temple Run");
 const newgameName = new String(gameName.substring(2,8))
 console.log(newgameName);
 console.log(gameName);
+const score = 400;
+console.log(score);
+const Balance = new Number(100)
+console.log(Balance.toFixed(2));
+console.log(Balance.toPrecision(1));
+console.log(Math.random())
+
 
 
