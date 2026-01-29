d5ae72a37534c6b2858fb28ef58029f92f52f0cc
shashwat12jha <shashwat1227@gmail.com>
2026-01-29 11:49:27 +0000

    Date and Time

---
+++ .devcontainer/01_Basica/dnt.js
@@ -0,0 +1,9 @@
+console.log("HelloBaby");
+// let myDate = new Date()
+// console.log(myDate.toDateString())
+// console.log(myDate.toLocaleString())
+let myDate = new Date(2026,0,29)
+console.log(myDate.toDateString())
+let present = Date.now();
+console.log(present);
+console.log(myDate.getTime());
